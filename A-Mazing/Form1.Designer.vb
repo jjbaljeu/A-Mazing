ae660bd3be860685d99e671237705f262306ca7b
jjbaljeu <jan.baljeu@pracsens.nl>
2021-04-25 18:07:31 +0200

    Initial

---
+++ A-Mazing/Form1.Designer.vb
@@ -0,0 +1,129 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ButtonLoadMaze = New System.Windows.Forms.Button()
+        Me.PictureBoxMazeInput = New System.Windows.Forms.PictureBox()
+        Me.ButtonAMaze = New System.Windows.Forms.Button()
+        Me.ButtonSaveMaze = New System.Windows.Forms.Button()
+        Me.NumericUpDownPrunning = New System.Windows.Forms.NumericUpDown()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.RichTextBoxMessages = New System.Windows.Forms.RichTextBox()
+        CType(Me.PictureBoxMazeInput, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDownPrunning, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ButtonLoadMaze
+        '
+        Me.ButtonLoadMaze.Location = New System.Drawing.Point(51, 31)
+        Me.ButtonLoadMaze.Name = "ButtonLoadMaze"
+        Me.ButtonLoadMaze.Size = New System.Drawing.Size(166, 46)
+        Me.ButtonLoadMaze.TabIndex = 0
+        Me.ButtonLoadMaze.Text = "Load Maze"
+        Me.ButtonLoadMaze.UseVisualStyleBackColor = True
+        '
+        'PictureBoxMazeInput
+        '
+        Me.PictureBoxMazeInput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBoxMazeInput.Location = New System.Drawing.Point(52, 112)
+        Me.PictureBoxMazeInput.Name = "PictureBoxMazeInput"
+        Me.PictureBoxMazeInput.Size = New System.Drawing.Size(378, 293)
+        Me.PictureBoxMazeInput.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBoxMazeInput.TabIndex = 1
+        Me.PictureBoxMazeInput.TabStop = False
+        '
+        'ButtonAMaze
+        '
+        Me.ButtonAMaze.Enabled = False
+        Me.ButtonAMaze.Location = New System.Drawing.Point(328, 31)
+        Me.ButtonAMaze.Name = "ButtonAMaze"
+        Me.ButtonAMaze.Size = New System.Drawing.Size(181, 46)
+        Me.ButtonAMaze.TabIndex = 2
+        Me.ButtonAMaze.Text = "A-Maze"
+        Me.ButtonAMaze.UseVisualStyleBackColor = True
+        '
+        'ButtonSaveMaze
+        '
+        Me.ButtonSaveMaze.Enabled = False
+        Me.ButtonSaveMaze.Location = New System.Drawing.Point(591, 31)
+        Me.ButtonSaveMaze.Name = "ButtonSaveMaze"
+        Me.ButtonSaveMaze.Size = New System.Drawing.Size(154, 46)
+        Me.ButtonSaveMaze.TabIndex = 3
+        Me.ButtonSaveMaze.Text = "Save Maze"
+        Me.ButtonSaveMaze.UseVisualStyleBackColor = True
+        '
+        'NumericUpDownPrunning
+        '
+        Me.NumericUpDownPrunning.Location = New System.Drawing.Point(668, 127)
+        Me.NumericUpDownPrunning.Maximum = New Decimal(New Integer() {500, 0, 0, 0})
+        Me.NumericUpDownPrunning.Name = "NumericUpDownPrunning"
+        Me.NumericUpDownPrunning.Size = New System.Drawing.Size(76, 26)
+        Me.NumericUpDownPrunning.TabIndex = 4
+        Me.NumericUpDownPrunning.Value = New Decimal(New Integer() {15, 0, 0, 0})
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(526, 127)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(127, 20)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Prunning passes"
+        '
+        'RichTextBoxMessages
+        '
+        Me.RichTextBoxMessages.Location = New System.Drawing.Point(462, 174)
+        Me.RichTextBoxMessages.Name = "RichTextBoxMessages"
+        Me.RichTextBoxMessages.Size = New System.Drawing.Size(315, 192)
+        Me.RichTextBoxMessages.TabIndex = 6
+        Me.RichTextBoxMessages.Text = ""
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.RichTextBoxMessages)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.NumericUpDownPrunning)
+        Me.Controls.Add(Me.ButtonSaveMaze)
+        Me.Controls.Add(Me.ButtonAMaze)
+        Me.Controls.Add(Me.PictureBoxMazeInput)
+        Me.Controls.Add(Me.ButtonLoadMaze)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBoxMazeInput, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDownPrunning, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ButtonLoadMaze As Button
+    Friend WithEvents PictureBoxMazeInput As PictureBox
+    Friend WithEvents ButtonAMaze As Button
+    Friend WithEvents ButtonSaveMaze As Button
+    Friend WithEvents NumericUpDownPrunning As NumericUpDown
+    Friend WithEvents Label1 As Label
+    Friend WithEvents RichTextBoxMessages As RichTextBox
+End Class
---
+++ A-Mazing/Form1.vb
@@ -0,0 +1,195 @@
+﻿Public Module GlobalVars
+    Public InputImage, AMazeImage As Bitmap
+End Module
+
+Structure Position
+    Dim X As Integer
+    Dim Y As Integer
+End Structure
+
+Public Class Form1
+    Private Sub ButtonLoadMaze_Click(sender As Object, e As EventArgs) Handles ButtonLoadMaze.Click
+        Dim ImageOpenDialog As New OpenFileDialog
+        ImageOpenDialog.Filter = "PNG|*.png|JPEG|*.jpg|Bitmap|*.bmp"
+        If ImageOpenDialog.ShowDialog = Windows.Forms.DialogResult.Cancel Then Exit Sub
+        Try
+            If Not IsNothing(InputImage) Then InputImage.Dispose()
+            InputImage = New Bitmap(ImageOpenDialog.FileName)
+            AMazeImage = New Bitmap(InputImage.Width, InputImage.Height)
+
+            Dim CurrentX, CurrentY As Integer
+            Dim InputPixelColor As Color
+
+            For CurrentX = 0 To InputImage.Width - 1
+                For CurrentY = 0 To InputImage.Height - 1
+                    InputPixelColor = InputImage.GetPixel(CurrentX, CurrentY)
+                    AMazeImage.SetPixel(CurrentX, CurrentY, InputPixelColor)
+                Next
+            Next
+
+            If Not IsNothing(PictureBoxMazeInput.Image) Then PictureBoxMazeInput.Image.Dispose()
+            PictureBoxMazeInput.Image = AMazeImage
+            ButtonSaveMaze.Enabled = False
+            ButtonAMaze.Enabled = True
+        Catch
+            MsgBox("Not a valid image file.")
+        End Try
+
+    End Sub
+
+    Private Sub ButtonAMaze_Click(sender As Object, e As EventArgs) Handles ButtonAMaze.Click
+
+        Dim CurrentPosition As Position
+        Dim NoOfExits, NoOfPasses, MaxPasses As Integer
+        Dim DeadEndsFound As Boolean
+        Dim StartTimePrunning, EndTimePrunning As Date
+
+        RichTextBoxMessages.Text = RichTextBoxMessages.Text & vbCrLf & "------------------"
+
+        MaxPasses = NumericUpDownPrunning.Value
+        If MaxPasses > 0 Then
+            NoOfPasses = 0
+            StartTimePrunning = DateTime.Now
+            Do
+                DeadEndsFound = False
+                For CurrentPosition.Y = 1 To AMazeImage.Height - 2
+                    For CurrentPosition.X = 1 To AMazeImage.Width - 2
+                        If (AMazeImage.GetPixel(CurrentPosition.X, CurrentPosition.Y).R = 255) Then
+                            NoOfExits = 4
+                            If AMazeImage.GetPixel(CurrentPosition.X, CurrentPosition.Y - 1).R = 0 Then NoOfExits = NoOfExits - 1
+                            If AMazeImage.GetPixel(CurrentPosition.X, CurrentPosition.Y + 1).R = 0 Then NoOfExits = NoOfExits - 1
+                            If AMazeImage.GetPixel(CurrentPosition.X - 1, CurrentPosition.Y).R = 0 Then NoOfExits = NoOfExits - 1
+                            If AMazeImage.GetPixel(CurrentPosition.X + 1, CurrentPosition.Y).R = 0 Then NoOfExits = NoOfExits - 1
+                            If NoOfExits <= 1 Then
+                                AMazeImage.SetPixel(CurrentPosition.X, CurrentPosition.Y, Color.FromArgb(0, 0, 128))
+                                DeadEndsFound = True
+                            End If
+                        End If
+                    Next
+                Next
+                NoOfPasses = NoOfPasses + 1
+            Loop While DeadEndsFound And NoOfPasses < MaxPasses
+            EndTimePrunning = DateTime.Now
+            RichTextBoxMessages.Text = RichTextBoxMessages.Text & vbCrLf & "Prunning passes: " & NoOfPasses
+            If NoOfPasses = MaxPasses Then RichTextBoxMessages.Text = RichTextBoxMessages.Text & " (limited)"
+            RichTextBoxMessages.Text = RichTextBoxMessages.Text & vbCrLf & "Prunning time: " & vbTab & (EndTimePrunning - StartTimePrunning).TotalMilliseconds & " mSec"
+        Else
+            RichTextBoxMessages.Text = RichTextBoxMessages.Text & vbCrLf & "No Prunning"
+        End If
+
+        Dim StartPositionMaze As Position
+        Dim Path(1), PathNew() As Position
+        Dim PathList As New List(Of Array)
+
+        'Locate the startposition in the first row
+        CurrentPosition.Y = 0
+        For CurrentPosition.X = 1 To AMazeImage.Width - 2
+            If (AMazeImage.GetPixel(CurrentPosition.X, CurrentPosition.Y).R = 255) Then
+                StartPositionMaze = CurrentPosition
+                Path(0) = StartPositionMaze
+                AMazeImage.SetPixel(StartPositionMaze.X, StartPositionMaze.Y, Color.FromArgb(0, 0, 255))
+                CurrentPosition.X = StartPositionMaze.X
+                CurrentPosition.Y = StartPositionMaze.Y + 1
+                Path(1) = CurrentPosition
+                PathList.Add(Path)
+                Exit For
+            End If
+        Next
+        If PathList.Count = 0 Then RichTextBoxMessages.Text = RichTextBoxMessages.Text & vbCrLf & "No start position found."
+
+        Dim StartTimeSolving, EndTimeSolving As Date
+
+        StartTimeSolving = DateTime.Now
+        Do While PathList.Count > 0
+            CurrentPosition = PathList(0)(PathList(0).Length - 1)
+
+            'Mark current position as 'no exit' i.e. path not available since it's already part of a path
+            AMazeImage.SetPixel(CurrentPosition.X, CurrentPosition.Y, Color.FromArgb(0, 0, 255))
+
+            'Scan surrounding for available exits i.e. possible paths if found then add this as a 
+            'new path to the list
+
+            'Check down position, it's the only possiblity for an end of maze
+            If (AMazeImage.GetPixel(CurrentPosition.X, CurrentPosition.Y + 1).R = 255) Then
+                PathNew = PathList(0)
+                Array.Resize(PathNew, PathNew.Length + 1)
+                PathNew(PathNew.Length - 1).X = CurrentPosition.X
+                PathNew(PathNew.Length - 1).Y = CurrentPosition.Y + 1
+                PathList.Add(PathNew)
+
+                'Check if the path exits at the end of the maze (last row), if so quit checking
+                If CurrentPosition.Y + 1 = AMazeImage.Height - 1 Then Exit Do
+            End If
+
+            'Check up position
+            If (AMazeImage.GetPixel(CurrentPosition.X, CurrentPosition.Y - 1).R = 255) Then
+                PathNew = PathList(0)
+                Array.Resize(PathNew, PathNew.Length + 1)
+                PathNew(PathNew.Length - 1).X = CurrentPosition.X
+                PathNew(PathNew.Length - 1).Y = CurrentPosition.Y - 1
+                PathList.Add(PathNew)
+            End If
+
+            'Check left position
+            If (AMazeImage.GetPixel(CurrentPosition.X - 1, CurrentPosition.Y).R = 255) Then
+                PathNew = PathList(0)
+                Array.Resize(PathNew, PathNew.Length + 1)
+                PathNew(PathNew.Length - 1).X = CurrentPosition.X - 1
+                PathNew(PathNew.Length - 1).Y = CurrentPosition.Y
+                PathList.Add(PathNew)
+            End If
+
+            'Check right position
+            If (AMazeImage.GetPixel(CurrentPosition.X + 1, CurrentPosition.Y).R = 255) Then
+                PathNew = PathList(0)
+                Array.Resize(PathNew, PathNew.Length + 1)
+                PathNew(PathNew.Length - 1).X = CurrentPosition.X + 1
+                PathNew(PathNew.Length - 1).Y = CurrentPosition.Y
+                PathList.Add(PathNew)
+            End If
+
+            'This path has been processed so remove it from the list
+            PathList.RemoveAt(0)
+        Loop
+
+        EndTimeSolving = DateTime.Now
+        RichTextBoxMessages.Text = RichTextBoxMessages.Text & vbCrLf & "Solving time: " & vbTab & (EndTimeSolving - StartTimeSolving).TotalMilliseconds & " mSec"
+        RichTextBoxMessages.Text = RichTextBoxMessages.Text & vbCrLf & "Total time: " & vbTab & ((EndTimePrunning - StartTimePrunning) + (EndTimeSolving - StartTimeSolving)).TotalMilliseconds & " mSec"
+
+        'The last path in the list contains the shortest route, color is path green
+        Dim CheckPosition As Position
+        If PathList.Count() > 0 Then
+            For PathCounter = 0 To PathList.Last().Length - 1
+                CheckPosition = PathList.Last()(PathCounter)
+                AMazeImage.SetPixel(CheckPosition.X, CheckPosition.Y, Color.FromArgb(0, 255, 0))
+            Next
+        Else
+            RichTextBoxMessages.Text = RichTextBoxMessages.Text & vbCrLf & "No end position found."
+        End If
+
+        'Update the screen and enable the option to save the solution
+        PictureBoxMazeInput.Refresh()
+        ButtonSaveMaze.Enabled = True
+        ButtonAMaze.Enabled = False
+    End Sub
+
+    Private Sub ButtonSaveMaze_Click(sender As Object, e As EventArgs) Handles ButtonSaveMaze.Click
+        Dim SaveDialog As New SaveFileDialog
+        SaveDialog.CreatePrompt = True
+        SaveDialog.DefaultExt = "jpg"
+        SaveDialog.Filter = "File Images (*.jpg;*.jpeg;) | *.jpg;*.jpeg; |PNG Images | *.png |GIF Images | *.GIF"
+        SaveDialog.InitialDirectory = "C:\Gegevens\Prive\Tekenen"
+
+        If SaveDialog.ShowDialog = Windows.Forms.DialogResult.OK Then
+            Try
+                AMazeImage.Save(SaveDialog.FileName)
+                ButtonSaveMaze.Enabled = False
+            Catch
+                MsgBox("Unable to save file.")
+            End Try
+
+        End If
+
+    End Sub
+
+End Class
